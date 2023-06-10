a07ba48228aa511b656253d82b32d3b341edd3ad
Deadpool <20658168+pwn01@users.noreply.github.com>
2023-06-11 01:17:01 +0530

    Create dp-gnome-vbox.sh

---
+++ dp-gnome-vbox.sh
@@ -0,0 +1,1 @@
+pacman -S xorg-server pulseaudio pulseaudio-alsa virtualbox-guest-utils xf86-video-vmware evince file-roller gdm gnome-backgrounds gnome-control-center gnome-console gnome-disk-utility gnome-font-viewer gnome-keyring gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-text-editor gnome-themes-extra nautilus sushi gnome-tweaks
