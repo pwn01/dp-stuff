06021bf6e3742b295721760bef340305c0ea6ee4
Deadpool <20658168+pwn01@users.noreply.github.com>
2023-06-11 01:28:29 +0530

    Update dp-gnome-vbox.sh

--- dp-gnome-vbox.sh
+++ dp-gnome-vbox.sh
@@ -1,1 +1,5 @@
-pacman -S xorg-server pulseaudio pulseaudio-alsa virtualbox-guest-utils xf86-video-vmware evince file-roller gdm gnome-backgrounds gnome-control-center gnome-console gnome-disk-utility gnome-font-viewer gnome-keyring gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-text-editor gnome-themes-extra nautilus sushi gnome-tweaks
+pacman -S xorg-server pulseaudio pulseaudio-alsa;
+pacman -S virtualbox-guest-utils xf86-video-vmware;
+systemctl enable vboxservice;
+pacman -S evince file-roller gdm gnome-backgrounds gnome-control-center gnome-console gnome-disk-utility gnome-font-viewer gnome-keyring gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-text-editor gnome-themes-extra nautilus sushi gnome-tweaks;
+systemctl enable gdm;
