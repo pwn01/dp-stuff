c5dede6833d43e00a4df7b55926c0f9153faedfd
Deadpool <20658168+pwn01@users.noreply.github.com>
2023-06-11 01:15:10 +0530

    Update dp-gnome.sh

--- dp-gnome.sh
+++ dp-gnome.sh
@@ -1,2 +1,2 @@
-sudo pacman -S evince file-roller gdm gnome-backgrounds gnome-control-center gnome-console gnome-disk-utility gnome-font-viewer gnome-keyring gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-text-editor gnome-themes-extra nautilus sushi gnome-tweaks
+pacman -S xorg-server pulseaudio pulseaudio-alsa mesa evince file-roller gdm gnome-backgrounds gnome-control-center gnome-console gnome-disk-utility gnome-font-viewer gnome-keyring gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-text-editor gnome-themes-extra nautilus sushi gnome-tweaks
 
